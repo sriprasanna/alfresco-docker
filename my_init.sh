174ebe9498d21a00243809fadf2f802a877d226a
Sri Prasanna <k.sriprasanna@gmail.com>
2015-01-23 14:10:13 -0500

    changing chown path

--- my_init.sh
+++ my_init.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-/usr/sbin/chown postgres -R /opt/alfresco/alf_data/postgresql
+/usr/bin/chown postgres -R /opt/alfresco/alf_data/postgresql
 /usr/sbin/service alfresco start
 /usr/bin/tail -f /opt/alfresco/tomcat/logs/*
